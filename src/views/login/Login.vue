612cd130d735e0eca469e3cc95e1a182eb39ca87
cgq666 <805696667@qq.com>
2020-03-30 17:03:05 +0800

    login

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,7 +1,17 @@
 <template>
-    <div>
-        <input type="text" v-model="username" />
-        <div @click="login">登陆</div>
+    <div class="login">
+        <!-- 登录面板 -->
+        <div class="login-box">
+            <div class="login-box-title">
+                后台管理系统
+            </div>
+            <div class="login-box-from">
+                 <input type="text" v-model="username" />
+                <div @click="login">登陆</div>
+            </div>
+           
+        </div>
+       
     </div>
    
 </template>
@@ -32,3 +42,36 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.login{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url('../../assets/image/login.jpg');
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+}
+.login-box{
+    width: 350px;
+    height: 287px;
+    background: hsla(0,0%,100%,.3);
+    border-radius: 5px;
+    box-shadow: 0 0 2px #f7f7f7;
+}
+.login-box-title{
+    line-height: 50px;
+    font-size: 20px;
+    color: #ffffff;
+    text-align: center;
+    border-bottom: 1px solid #ffffff;
+}
+.login-box-from{
+    width: 100%;
+    height: auto;
+    padding: 30px;
+    box-sizing: border-box;
+}
+</style>
